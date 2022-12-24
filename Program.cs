b623ae3be3bf0409f62e51a16906e59a282da4aa
dhwlsdn1001 <ojw061001@naver.com>
2022-12-24 13:07:44 +0900

    2022/12/24-json complete

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace genshin_gotcha
 {
@@ -15,9 +16,18 @@ namespace genshin_gotcha
     {
         static void Main(string[] args)
         {
-            
+            string json;
+            string datapath = "C:\\projects\\genshin_gotcha\\data";//언제든 변경 가능
+            using (StreamReader r = new StreamReader(datapath + "\\4_grade_character.json"))
+            {
+                json = r.ReadToEnd();
+            }
+            List<Character> ch = JsonConvert.DeserializeObject<List<Character>>(json);
+            for (int i = 0; i < ch.Count; i++)
+            {
+
+                Console.Write("\"" + ch[i].name + "\", ");
+            }
         }
-
-
     }
 }
